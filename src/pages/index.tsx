0f8d1d30002e77e9ce06025b1f47995b58fdaea0
Vítor Marco <vitoradrianomarco@gmail.com>
2022-07-29 09:01:50 -0300

    Add banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import { Header } from '../components/Header';
 import { Main } from '../template/Main';
+import { imagesPaths } from '../utils';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      {/* <Banner /> */}
+      <Box w="100%" position="relative">
+        <Image
+          src={imagesPaths.sky}
+          width="1440px"
+          height="335px"
+          layout="responsive"
+        />
+      </Box>
       <Main>
         <h1>hello</h1>
       </Main>
--- src/utils/images/paths.ts
+++ src/utils/images/paths.ts
@@ -1,3 +1,4 @@
 export const imagesPaths = {
   logo: '/logo.svg',
+  sky: '/images/banner.png',
 };

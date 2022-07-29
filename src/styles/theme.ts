df8e4223ad4d7c078c36140c0224cd76f76a77de
Vítor Marco <vitoradrianomarco@gmail.com>
2022-07-28 21:16:56 -0300

    install chakra

---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      200: '#DADADA',
+      400: '#999999',
+      600: '#47585B',
+    },
+    yellow: {
+      100: '#FFBA08',
+    },
+    white: {
+      light: '#ffffff',
+      100: '#F5F8FA',
+    },
+    black: {
+      dark: '#000000',
+    },
+    red: {
+      900: '1C1401',
+    },
+  },
+  fonts: {
+    heading: ['Poppins', 'sans-serif'],
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white.100',
+        color: 'gray.600',
+      },
+    },
+  },
+});
